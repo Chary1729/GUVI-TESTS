77bad22cef5196cfb6dcd661a6adae4e05afea4b
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-03-19 20:00:43 +0530

    Update larg.py

--- larg.py
+++ larg.py
@@ -1,9 +1,9 @@
 x=int(input())
 y=int(input())
 z=int(input())
-if (x>=y) and (x>=z):
-    print(x)
-elif (y>=x) and (y>=z):
-    print(y)
+if (x>y>z):
+    print (x)
+elif (y>x>z):
+    print (y)
 else:
-    print(z)
+    print (z)
