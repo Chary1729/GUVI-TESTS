b90dd5466cde9de8a4e8686db569106e6199600c
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-03-19 19:23:30 +0530

    Create char.py

---
+++ char.py
@@ -0,0 +1,12 @@
+ch = input()
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print("Vowel")
+elif(ch=='B' or ch=='b' or ch=='C' or ch =='c' or ch=='D'
+ or ch=='d' or ch=='F' or ch=='f' or ch=='g' or ch=='G' or ch=='H' or ch=='h' or ch=='j' or ch =='J' or ch=='K'
+ or ch=='k' or ch=='L' or ch=='l' or ch=='M' or ch=='m' or ch=='N' or ch=='n' or ch=='P' or ch =='p' or ch=='Q'
+ or ch=='q' or ch=='R' or ch=='r' or ch=='S' or ch=='s' or ch=='T' or ch=='t' or ch=='V' or ch =='v' or ch=='W'
+ or ch=='w' or ch =='x' or ch=='X' or ch=='Y' or ch=='y' or ch=='z' or ch=='Z'):
+    print("Consonant")
+else:
+    print("invalid")
