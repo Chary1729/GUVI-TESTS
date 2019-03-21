edd1952e4c583e9c066a57ef1d769a7ca20642a2
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-03-21 12:34:40 +0530

    Create lar.py

---
+++ lar.py
@@ -0,0 +1,9 @@
+num1,num2,num3 = int(input()), int(input()),int(input())
+
+ 
+if (num1 > num2) and (num1 > num3):
+                           print(num1)
+elif (num2 > num1) and (num2 > num3):
+                           print(num2)
+else:
+                           print(num3)
